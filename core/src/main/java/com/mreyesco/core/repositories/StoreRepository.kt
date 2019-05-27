aa0563809a95395dd768e2b1724ebbaa4d9b8985
Chelo <marceloreyescorrea@gmail.com>
2019-05-26 19:02:05 -0500

    feat: Add product item layout

--- core/src/main/java/com/mreyesco/core/repositories/StoreRepository.kt
+++ core/src/main/java/com/mreyesco/core/repositories/StoreRepository.kt
@@ -3,6 +3,7 @@ package com.mreyesco.core.repositories
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.mreyesco.core.model.ProductModel
 import com.mreyesco.core.model.ProductModelInterface
 import com.mreyesco.core.networking.ProductsApi
 import io.reactivex.Observable
@@ -11,8 +12,8 @@ class StoreRepository(private val productsApi: ProductsApi) {
     fun getProducts(): Observable<List<ProductModelInterface>> {
         return productsApi.getProducts()
             .map {
-                val type = object : TypeToken<List<ProductModelInterface>>() {}.type
-                Gson().fromJson<List<ProductModelInterface>>(it, type)
+                val type = object : TypeToken<List<ProductModel>>() {}.type
+                Gson().fromJson<List<ProductModel>>(it, type)
             }
     }
 }
