c066187e4139a2baee8dec071abaeabe6d3627f2
Chelo <marceloreyescorrea@gmail.com>
2019-05-29 22:12:38 -0500

    feat: Add binding adapter to load images on imageView

--- store/src/main/java/com/mreyesco/store/view/BindingAdapters.kt
+++ store/src/main/java/com/mreyesco/store/view/BindingAdapters.kt
@@ -1,8 +1,10 @@
 package com.mreyesco.store.view
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.mreyesco.store.R
 
 @BindingAdapter(value = ["amountAdded"])
@@ -16,4 +18,11 @@ fun setAmountAdded(view: TextView, amount: Int) {
 @BindingAdapter("app:goneUnless")
 fun goneUnless(view: View, visible: Boolean) {
     view.visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("app:loadImage")
+fun loadImage(view: ImageView, url: String) {
+    if (url.isNotEmpty()) {
+        Glide.with(view.context).load(url).into(view)
+    }
 }
